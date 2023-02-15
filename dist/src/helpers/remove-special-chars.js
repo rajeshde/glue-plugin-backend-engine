8e112742b0b8f87ad45fe0d083072d6993c8aa9e
Faiz A. Farooqui <faizz.af@gmail.com>
2023-02-15 17:21:42 +0530

    chore: removed unused helpers

--- dist/src/helpers/remove-special-chars.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.removeSpecialChars = void 0;
-var removeSpecialChars = function (str) {
-    return str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-};
-exports.removeSpecialChars = removeSpecialChars;
-//# sourceMappingURL=remove-special-chars.js.map
