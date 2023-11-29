aa5cda979a923fe66df1cd5cf8d68a5efc469f28
rajeshde <rajeshd422@gmail.com>
2023-11-29 16:37:37 +0530

    Replaced Dapr invocation with axios call

--- packages/cli/template/src/call-manager/sync-functions.js
+++ packages/cli/template/src/call-manager/sync-functions.js
@@ -1,13 +1,6 @@
-const { DaprClient, HttpMethod } = require('@dapr/dapr');
-
 module.exports = async (callbacks, payload) => {
 
   console.log({callbacks, payload});
-
-  const daprPort = 3500;
-  const daprHost = '127.0.0.1';
-
-  const client = new DaprClient(daprHost, daprPort);
 
   for await (const callback of callbacks) {
     const { value } = callback;
@@ -19,13 +12,19 @@ module.exports = async (callbacks, payload) => {
     }
 
     try {
-      await client.invoker.invoke(
-        serviceAppId.replace(/-/g, ''),
-        serviceMethod,
-        HttpMethod.POST,
-        { ...payload },
-        {}
-      );
+      const appId = serviceAppId.replace(/-/g, '');
+
+      await axios({
+        method: 'post',
+        url: `http://${appId}:${process.env.APP_PORT}/${serviceMethod}`,
+        data: {
+          action_name: appId,
+          method_uri: serviceMethod,
+          method_name: 'post',
+          data: { ...payload },
+        },
+        headers: {},
+      });
     } catch (err) {
       console.log(`Error invoking ${serviceAppId}::${serviceMethod}: ${err}`);
       continue;
