94fdf84c1ea78078d5685002561b600793a15f43
rajeshde <rajeshd422@gmail.com>
2023-11-30 12:52:43 +0530

    moved the entire contents of packages/cli in the root level. It is just for testing and will be reverted back.

--- packages/cli/dist/src/configs/constants.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const backendPlugins: string[];
-export declare const ignorePlugins: string[];
-export declare const noDockerfiles: string[];
-export declare const daprServices: string[];
