7540b3d1a5e9b1efe3877f6fa474970bf8bc5c0c
rajeshde <rajeshd422@gmail.com>
2023-12-04 15:46:19 +0530

    Added missing template folder inside packages/cli folder

---
+++ packages/cli/dist/src/helpers/valid-glue-service.d.ts
@@ -0,0 +1,3 @@
+export declare const isValidGluePlugin: (backendPlugins: string[], name: string) => string[];
+export declare const isDaprService: (name: string) => boolean;
+export declare const isGlueService: (name: string) => boolean;
